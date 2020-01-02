d378f8bb92513b273f552501f94f4c6a3d4fa048
niart120(home) <axe0120@gmail.com>
2020-01-02 18:16:49 +0900

    Update Main.java

--- src/seedfinder/Main.java
+++ src/seedfinder/Main.java
@@ -18,25 +18,26 @@ public class Main {
 
 		RevMatBuilder rmb = new RevMatBuilder();
 		RevMat rm = rmb.build(0);
-		SeedFinder sf = new SeedFinder(rm);
+		SeedCollector clctr = new SeedCollector(rm,1,observed,puzzle);
 
 		long[] sndivs = {18,31,31,31,14,31};
 		long sndabi = 0;
 		long sndgen = 2;
-
 		long sndntr = Nature.getByJa("ようき");
-
 		Pokemon second = new Pokemon(sndivs, sndabi, sndgen,sndntr);
 
 		long[] trdivs = {10,0,31,31,9,31};
 		long trdabi = 1;
 		long trdgen = 2;
 		long trdntr = Nature.getByJa("れいせい");
-
 		Pokemon third = new Pokemon(trdivs, trdabi, trdgen,trdntr);
 
 		GeneratorBuilder gb = new GeneratorBuilder(3, true, true, 3, 0);
-		ArrayList<Long> seedlist = sf.find(1L,observed,puzzle,second, third,gb);
+
+		SeedChecker chkr = new SeedChecker(second,third,gb);
+
+		SeedFinder sf = new SeedFinder(clctr, chkr);
+		ArrayList<Long> seedlist = sf.find();
 
 		for(Long l: seedlist) {
 			System.out.println(l);
