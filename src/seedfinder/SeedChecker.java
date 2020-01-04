6e9f5e8b55a763830581d6988c93be63c50ef9a2
niart120(home) <axe0120@gmail.com>
2020-01-05 01:00:44 +0900

    Update SeedChecker.java
    added forth checker

--- src/seedfinder/SeedChecker.java
+++ src/seedfinder/SeedChecker.java
@@ -3,20 +3,30 @@ package seedfinder;
 public class SeedChecker {
 	Pokemon second;
 	Pokemon third;
-	GeneratorBuilder gb;
+	Pokemon forth;
+	GeneratorBuilder gb1,gb2,gb3;
 
-	public SeedChecker(Pokemon second, Pokemon third, GeneratorBuilder gb) {
+
+	public SeedChecker(Pokemon second, Pokemon third, Pokemon forth, GeneratorBuilder gb1, GeneratorBuilder gb2, GeneratorBuilder gb3) {
 		this.second = second;
 		this.third = third;
-		this.gb = gb;
+		this.forth = forth;
+		this.gb1 = gb1;
+		this.gb2 = gb2;
+		this.gb3 = gb3;
 	}
 
 	public boolean checkSeed(long seed) {
-		Generator g = gb.build(seed);
-		Pokemon secondCheck = g.genCheck();
+		Generator g1 = gb1.build(seed);
+		Generator g2 = gb2.build(seed+0x82A2B175229D6A5BL);
+		Generator g3 = gb3.build(seed+0x82A2B175229D6A5BL+0x82A2B175229D6A5BL);
+		Pokemon secondCheck = g1.genCheck();
 		if(secondCheck.equals(second)) {
-			Pokemon thirdCheck = g.genCheck();
-			return thirdCheck.equals(third);
+			Pokemon thirdCheck = g2.genCheck();
+			if(thirdCheck.equals(third)) {
+				Pokemon forthCheck = g3.genCheck();
+				return forthCheck.equals(forth);
+			}
 		}
 		return false;
 	}
